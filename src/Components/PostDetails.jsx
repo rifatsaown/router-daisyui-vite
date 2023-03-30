e6bc50aa8d10bbfc9a26c88e1b26eeb81ae9b57d
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-03-30 22:19:30 +0600

    done

--- src/Components/PostDetails.jsx
+++ src/Components/PostDetails.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PostDetails = () => {
     return (
         <div>
-            Hello This is Post Details
+            This is Post Details
         </div>
     );
 };
